55e8f83ff1c77a12c3cd6dd9154573f5fdfdaae7
tstst334@gmail.com <thorstenstock334@gmail.com>
2024-09-01 23:41:15 +0200

    guess who is too stupid to write Pictures correctly. Works just fine now

--- hypr/scripts/changeBackground.sh
+++ hypr/scripts/changeBackground.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-directory=/home/$USER/Pitures/wp
+directory=/home/$USER/Pictures/wp
 monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 if [ -d "$directory" ]; then
     random_background=$(ls $directory/*.jpg | shuf -n 1)
+   
 
     hyprctl hyprpaper unload all
     hyprctl hyprpaper preload $random_background
